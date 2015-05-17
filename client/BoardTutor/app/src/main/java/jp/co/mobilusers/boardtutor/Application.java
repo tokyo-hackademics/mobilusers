b08b70fcdd18ad5dbc99a5bc6566c30309ec0784
Dat Do <datdvt@gmail.com>
2015-05-17 13:10:13 +0900

    App: add ongoing notification when app starts

--- client/BoardTutor/app/src/main/java/jp/co/mobilusers/boardtutor/Application.java
+++ client/BoardTutor/app/src/main/java/jp/co/mobilusers/boardtutor/Application.java
@@ -1,11 +1,22 @@
 package jp.co.mobilusers.boardtutor;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+
 import com.datdo.mobilib.base.MblBaseApplication;
 import com.datdo.mobilib.event.MblCommonEvents;
 import com.datdo.mobilib.event.MblEventCenter;
 import com.datdo.mobilib.event.MblEventListener;
 
+import java.util.Random;
+
 import jp.co.mobilusers.boardmessenger.BoardMessenger;
+import jp.co.mobilusers.boardtutor.activity.MainActivity;
+import jp.co.mobilusers.boardtutor.activity.MainActivity_;
 import jp.co.mobilusers.boardtutor.auth.GoogleApi;
 
 
@@ -23,6 +34,32 @@ public class Application extends MblBaseApplication implements MblEventListener 
                 MblCommonEvents.GO_TO_BACKGROUND,
                 MblCommonEvents.GO_TO_FOREGROUND
         });
+
+        addNotificationToStatusBar();
+    }
+
+    private void addNotificationToStatusBar() {
+
+        Intent intent = new Intent(getApplicationContext(), MainActivity_.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+                getApplicationContext(),
+                (new Random()).nextInt(),
+                intent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(getString(R.string.app_name))
+                .setContentText(getString(R.string.let_draw))
+                .setContentIntent(pendingIntent)
+                .setOngoing(true)
+                .setWhen(System.currentTimeMillis());
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.cancelAll();
+        notificationManager.notify("boardtutor", 0, builder.build());
     }
 
     @Override
