ac371af41dfca1e17399ff0c2591d939fa813c75
knight <knighthedspi@gmail.com>
2015-05-17 13:36:37 +0900

    Client: add user icon, update button

--- client/BoardTutor/app/src/main/java/jp/co/mobilusers/boardtutor/activity/RenderBoardActivity.java
+++ client/BoardTutor/app/src/main/java/jp/co/mobilusers/boardtutor/activity/RenderBoardActivity.java
@@ -3,21 +3,32 @@ package jp.co.mobilusers.boardtutor.activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.SeekBar;
 
 import com.datdo.mobilib.util.MblUtils;
+import com.squareup.picasso.Picasso;
 
+import org.androidannotations.annotations.AfterInject;
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
 
+import java.util.Arrays;
+import java.util.List;
+
+import jp.co.mobilusers.boardmessenger.BoardMessenger;
+import jp.co.mobilusers.boardmessenger.model.Board;
 import jp.co.mobilusers.boardmessenger.render.RenderBoard;
 import jp.co.mobilusers.boardtutor.R;
+import jp.co.mobilusers.boardtutor.auth.GoogleApi;
+import jp.co.mobilusers.boardtutor.model.User;
 import yuku.ambilwarna.AmbilWarnaDialog;
 
 /**
@@ -40,6 +51,28 @@ public class RenderBoardActivity extends BaseActivity {
     @ViewById(R.id.select_color_button)
     View selectColorButton;
 
+    @ViewById(R.id.userIcon1)
+    ImageView userIcon1;
+
+    @ViewById(R.id.userIcon2)
+    ImageView userIcon2;
+
+    @ViewById(R.id.userIcon3)
+    ImageView userIcon3;
+
+    @ViewById(R.id.userIcon4)
+    ImageView userIcon4;
+
+    @ViewById(R.id.userIcon5)
+    ImageView userIcon5;
+
+    String currentUserId;
+
+    @AfterInject
+    void init(){
+        currentUserId = BoardMessenger.getInstance().getAccount().getUserId();
+    }
+
     @AfterViews
     void initView(){
         mBoardId = getIntent().getStringExtra(BOARD_ID);
@@ -51,6 +84,7 @@ public class RenderBoardActivity extends BaseActivity {
         FrameLayout renderBoardContainer = (FrameLayout) findViewById(R.id.render_board_container);
         renderBoardContainer.addView(mRenderBoard);
         freeModeButton.performClick();
+        showMemberIcon();
     }
 
     @Click(R.id.free_mode_button)
@@ -146,5 +180,34 @@ public class RenderBoardActivity extends BaseActivity {
         });
 
         dialog.show();
+    }
+
+    void showMemberIcon() {
+        Board boardInfo = BoardMessenger.getInstance().getBoard(mBoardId);
+        String[] memberIds = boardInfo.getMembers();
+        List<User> userList = User.get(Arrays.asList(memberIds));
+        final ImageView[] userIcons = {userIcon1, userIcon2, userIcon3, userIcon4, userIcon5};
+        int limit = userList.size() <= 5 ? userList.size() : 5;
+        for(int i = 0; i < limit; i ++) {
+            if(userList.get(i).getId().equals(currentUserId)){
+                continue;
+            }
+            String userIconPath = userList.get(i).getThumbnail();
+            final int index = i;
+            GoogleApi.downloadImage(userIconPath, new GoogleApi.DownloadImageCallback() {
+                @Override
+                public void onSuccess(String path) {
+                    if (!path.startsWith("file:///")) {
+                        path = "file:///" + path;
+                    }
+                    Picasso.with(MblUtils.getCurrentContext()).load(path).into(userIcons[index]);
+                }
+
+                @Override
+                public void onError() {
+
+                }
+            });
+        }
     }
 }
