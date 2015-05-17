96953ca9d43e9d8c75c18506582f968bc97de964
knight <knighthedspi@gmail.com>
2015-05-17 10:24:10 +0900

    Client : show nick name in board adapter

--- client/BoardTutor/app/src/main/java/jp/co/mobilusers/boardtutor/adapter/BoardAdapter.java
+++ client/BoardTutor/app/src/main/java/jp/co/mobilusers/boardtutor/adapter/BoardAdapter.java
@@ -10,14 +10,17 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.datdo.mobilib.base.MblBaseAdapter;
-import com.datdo.mobilib.util.MblSimpleImageLoader;
 import com.datdo.mobilib.util.MblUtils;
 import com.squareup.picasso.Picasso;
+
+import java.util.Arrays;
+import java.util.List;
 
 import jp.co.mobilusers.boardmessenger.BoardMessenger;
 import jp.co.mobilusers.boardmessenger.model.Board;
 import jp.co.mobilusers.boardtutor.R;
 import jp.co.mobilusers.boardtutor.activity.RenderBoardActivity_;
+import jp.co.mobilusers.boardtutor.model.User;
 
 public class BoardAdapter extends MblBaseAdapter<Board> {
 
@@ -35,8 +38,16 @@ public class BoardAdapter extends MblBaseAdapter<Board> {
 
         view.setTag(board);
 
+        String [] userNameList;
+        if(view.getTag(R.string.member_key) == null) {
+            userNameList = getMemberNames(board.getMembers());
+            view.setTag(R.string.member_key, userNameList);
+        } else {
+            userNameList = (String []) view.getTag(R.string.member_key);
+        }
+
         ((TextView) view.findViewById(R.id.name_text)).setText(board.getName());
-        ((TextView) view.findViewById(R.id.members_text)).setText(TextUtils.join(",", board.getMembers()));
+        ((TextView) view.findViewById(R.id.members_text)).setText(TextUtils.join(", ", userNameList));
 
         view.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,5 +64,17 @@ public class BoardAdapter extends MblBaseAdapter<Board> {
                 .into((ImageView) view.findViewById(R.id.thumbnail_image));
 
         return view;
+    }
+
+    private String[] getMemberNames(String[] memberIds) {
+        if(memberIds == null || memberIds.length == 0){
+            return null;
+        }
+        String[] memberNames = new String[memberIds.length];
+        List<User> userList = User.get(Arrays.asList(memberIds));
+        for(int i = 0; i < memberIds.length; i++) {
+            memberNames[i] = userList.get(i).getNickname();
+        }
+        return memberNames;
     }
 }
